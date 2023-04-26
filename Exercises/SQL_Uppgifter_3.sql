249220ebec4866afc967e00eab66810390366af8
Canesp <philip.decanesie@gmail.com>
2023-04-26 08:56:42 +0200

    added some spacing

--- Exercises/SQL_Uppgifter_3.sql
+++ Exercises/SQL_Uppgifter_3.sql
@@ -8,7 +8,8 @@ PRINT(@London_products / @Num_products)
 
 /* 2. Till vilken stad har vi levererat flest unika produkter? */
 
-SELECT o.ShipCity, COUNT(DISTINCT od.ProductId) as 'Unique products' FROM company.orders o JOIN company.order_details od on o.id = od.OrderId GROUP BY o.ShipCity ORDER BY 'Unique products' DESC;
+SELECT TOP 1 o.ShipCity, COUNT(DISTINCT od.ProductId) as 'Unique products' FROM company.orders o JOIN company.order_details od on o.id = od.OrderId GROUP BY o.ShipCity ORDER BY 'Unique products' DESC;
+
 
 /* 3. Av de produkter som inte längre finns I vårat sortiment, hur mycket har vi sålt för totalt till Tyskland? */
 
@@ -18,6 +19,7 @@ SELECT ROUND(SUM(od.UnitPrice * od.Quantity), 2) as 'Sold ($)' FROM company.orde
 /* 4. För vilken produktkategori har vi högst lagervärde? */
 
 SELECT p.CategoryId, ROUND(SUM(p.UnitPrice * p.UnitsInStock), 2) as 'Value' FROM company.categories c JOIN company.products p on c.Id = p.CategoryId GROUP BY p.CategoryId ORDER BY 'Value' DESC;
+
 
 /* 5. Från vilken leverantör har vi sålt flest produkter totalt under sommaren 2013? */
 
